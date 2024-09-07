b0067b9a7abd7a4b6e5379edb428c48d62b3585c
Sayhan Ahmed <sayhan@ewnbd.com>
2024-09-07 19:06:54 +0600

    nav

---
+++ src/Components/organisms/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-[#0E0E0E] py-[60px]'>
+        <div className='container mx-auto'>
+            
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
